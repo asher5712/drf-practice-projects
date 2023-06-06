86f448598765a8d7c07df4cf8a09a7663b844d3c
Sher Ali <asher93905@gmail.com>
2023-06-06 23:21:13 +0500

    Authentication (Basic) project added

---
+++ BasicAuthentication - DRFProject/watchlist/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission, IsAdminUser, SAFE_METHODS
+
+class IsAdminOrReadOnly(IsAdminUser):
+    
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        
+        return bool(request.user and request.user.is_staff)
+    
+    class ReviewUserorReadOnly(BasePermission):
+        def has_object_permission(self, request, view, obj):
+            if request.method in SAFE_METHODS:
+                return True
+            else:
+                return obj.review_user == request.user
