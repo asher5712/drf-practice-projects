b808e1ddd87f9e43c53d2bacb3e66f9f3aa8175b
Sher Ali <asher93905@gmail.com>
2023-06-06 23:18:50 +0500

    Generic Views (Concrete) project added

---
+++ GenericConcreteViews - DRFProject/watchlist/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('list', views.WatchListAV.as_view(), name='watch-list'),
+    path('<int:pk>', views.WatchListDetailAV.as_view(), name='watch-details'),
+    path('platform', views.PlatformListAV.as_view(), name='platform-list'),
+    path('platform/<int:pk>', views.PlatformDetailAV.as_view(), name='platform-details'),
+    path('platform/<int:pk>/review', views.ReviewList.as_view(), name='review-list'),
+    path('platform/<int:pk>/review-create', views.ReviewCreate.as_view(), name='review-create'),
+    path('platform/review/<int:pk>', views.ReviewDetail.as_view(), name='review-details'),
+]
